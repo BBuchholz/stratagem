1a87242c8b3235094bf094656f74ebb32c589320
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-11 20:42:49 -0500

    Add font

--- src/pages/myrKiSs/CGBA.vue
+++ src/pages/myrKiSs/CGBA.vue
@@ -1,15 +1,22 @@
 <template>
-  <p>
+  <p class="handwritten">
     "It started as an experiment in the woods, a chance
     to plumb the depths of our souls and tap into that
-    ancient source of awe-ful grace, an opportunity to reach
+    ancient source of awe-ful grace.
+    <br><br>
+    An opportunity to reach
     into the heart of darkness and experience life
     on an entirely new level of consciousness and connection.
     Innocently, it was a hope for a better grasp
     on the energies that shape our lives
-    and in turn a better life journey. We had
-    no idea how naive we were. We had no idea how
-    unprepared we would be for what we found. We had
+    and in turn a better life journey.
+    <br><br>
+    We had no idea how naive we were.
+    <br><br>
+    We had no idea how
+    unprepared we would be for what we found.
+    <br><br>
+    We had
     no idea what we would bring back with us..."
   </p>
   <div>
@@ -21,6 +28,13 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+
+  .handwritten {
+    font-family: 'Caveat', cursive;
+    font-size: 36px;
+  }
+
   .card {
     max-width: 50%;
     height: auto;
