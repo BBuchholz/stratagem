43fe95bdc6684b3d90552a00a02b4346e72e78d4
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-30 06:27:15 -0500

    RELEASE

---
+++ src/pages/myrKiSs/AKLO.vue
@@ -0,0 +1,29 @@
+<template>
+  <p class="display">
+    Welcome to the Members Area, here you can get access to current and previous issues of thaStrataGem, as well as request access to templates for all remixables within tha Myriad EcoSystem Arrangement
+  </p>
+  <ul class="display">
+    <li>
+      <a
+        target="_blank"
+        href="https://madamadam.s3.us-east-2.amazonaws.com/2023OCT_thaStrataGem.pdf"
+      >
+        2023OCT_thaStrataGem
+      </a>
+    </li>
+    <li>
+      <a
+        target="_blank"
+        href="mailto:syndicated.circuitry@gmail.com"
+      >
+        request template access (ask your Collective's Treasurer for details)
+      </a>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  .display {
+    margin: 40px;
+  }
+</style>
