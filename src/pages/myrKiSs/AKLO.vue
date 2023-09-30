1c5eca6d185f01d9ca0a38ce3f7297e8e5c02889
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-30 06:39:42 -0500

    add more details to members page

--- src/pages/myrKiSs/AKLO.vue
+++ src/pages/myrKiSs/AKLO.vue
@@ -2,21 +2,30 @@
   <p class="display">
     Welcome to the Members Area, here you can get access to current and previous issues of thaStrataGem, as well as request access to templates for all remixables within tha Myriad EcoSystem Arrangement
   </p>
+  <p class="display">
+    Keep in mind that members are THREE MONTHS AHEAD of tha general public with regards to the StrataGem, use this knowledge accordingly.
+  </p>
+  <p class="display">
+    Approaching Senior Members with the right ideas now, can greatly alter your experience 12 weeks out, note that well...
+  </p>
+  <p class="display">
+    As always, any questions should be directed to Senior Members at appropriate times (during weekly collaboration sessions is usually best)
+  </p>
   <ul class="display">
-    <li>
+    <li class="bordered">
       <a
         target="_blank"
         href="https://madamadam.s3.us-east-2.amazonaws.com/2023OCT_thaStrataGem.pdf"
       >
-        2023OCT_thaStrataGem
+        DOWNLOAD tha StrataGem OCT 2023 (INAUGURAL ISSUE!)
       </a>
     </li>
-    <li>
+    <li class="bordered">
       <a
         target="_blank"
         href="mailto:syndicated.circuitry@gmail.com"
       >
-        request template access (ask your Collective's Treasurer for details)
+        request template access (ask a Senior Member for details)
       </a>
     </li>
   </ul>
@@ -25,5 +34,13 @@
 <style scoped>
   .display {
     margin: 40px;
+  }
+
+  .bordered {
+    margin: 40px;
+    color: red;
+    border-width: 1px;
+    border-style: solid;
+    border-color: gray;
   }
 </style>
